bb3c1d0d05cdca0916a2a7f6c0bdb0bbc20a5de0
gdouzas <gdouzas@icloud.com>
2017-05-24 01:00:56 +0300

    Restructered main loop

--- imbtools/evaluation.py
+++ imbtools/evaluation.py
@@ -61,38 +61,37 @@ class BinaryExperiment:
     def _initialize_parameters(self):
         """Private method that initializes the experiment's parameters."""
 
-        # Convert metrics to scorers
-        self.scorers_ = [make_scorer(metric) if metric is not roc_auc_score else make_scorer(metric, needs_threshold=True) for metric in self.metrics]
-
         # Read csv files and save them to a dictionary
         if isinstance(self.datasets, str):
             chdir(self.datasets)
-            self.datasets = {}
+            self.datasets_ = {}
             csv_files = [csv_file for csv_file in listdir() if match('^.+\.csv$', csv_file)]
             for csv_file in csv_files:
                 dataset = pd.read_csv(csv_file)
                 X, y = dataset.iloc[:, :-1], dataset.iloc[:, -1]
                 dataset_name = sub(".csv", "", csv_file)
-                self.datasets[dataset_name] = (X, y)
+                self.datasets_[dataset_name] = (X, y)
 
         # If a list of (X,y) data is given, append names to each one of them
         if isinstance(self.datasets, list):
-            self.datasets = {("dataset_" + str(ind + 1)):dataset for ind, dataset in enumerate(self.datasets)}
+            self.datasets_ = {("dataset_" + str(ind + 1)):dataset for ind, dataset in enumerate(self.datasets)}
         
         # Create random states for experiments
         self.random_states_ = [self.random_state * index for index in range(self.experiment_repetitions)] if self.random_state is not None else [None] * self.experiment_repetitions
         
         # Extract names for experiments parameters
-        self.classifiers_names_ = count_elements([classifier.__class__.__name__ for classifier in self.classifiers])
-        self.oversampling_methods_names_ = count_elements([oversampling_method.__class__.__name__ if oversampling_method is not None else 'None' for oversampling_method in self.oversampling_methods])
-        self.metrics_names_ = [sub('_', ' ', metric.__name__) for metric in self.metrics]
-        self.datasets_names_ = self.datasets.keys()
+        self.classifiers_ = dict(zip(count_elements([classifier.__class__.__name__ for classifier in self.classifiers]), self.classifiers))
+        self.oversampling_methods_ = dict(zip(count_elements([oversampling_method.__class__.__name__ if oversampling_method is not None else 'None' for oversampling_method in self.oversampling_methods]), self.oversampling_methods))
+        self.metrics_ = dict(zip([sub('_', ' ', metric.__name__) for metric in self.metrics], self.metrics))
+        
+        # Converts metrics to scores
+        self.scorers_ = dict(zip(self.metrics_.keys(), [make_scorer(metric) if metric is not roc_auc_score else make_scorer(metric, needs_threshold=True) for metric in self.metrics]))
 
     def _summarize_datasets(self):
         """Creates a summary of the datasets."""
         summary_columns = ["Dataset name", "# of features", "# of instances", "# of minority instances", "# of majority instances", "Imbalanced Ratio"]
         self.datasets_summary_ = pd.DataFrame({}, columns=summary_columns)
-        for dataset_name, (X, y) in self.datasets.items():
+        for dataset_name, (X, y) in self.datasets_.items():
             n_instances = ((y == 0).sum(), (y == 1).sum())
             dataset_summary = pd.DataFrame([[dataset_name, X.shape[1], y.size, n_instances[1], n_instances[0], round(n_instances[0] / n_instances[1], 2)]], columns=self.datasets_summary_.columns)
             self.datasets_summary_ = self.datasets_summary_.append(dataset_summary, ignore_index=True)
@@ -108,23 +107,19 @@ class BinaryExperiment:
         results = pd.DataFrame(columns=['Dataset', 'Classifier', 'Oversampling method', 'Metric', 'CV score'])
         for experiment_ind, random_state in enumerate(self.random_states_):
             cv = StratifiedKFold(n_splits=self.n_splits, random_state=random_state, shuffle=True)
-            for clf_ind, clf in enumerate(self.classifiers):
+            for classifier_name, clf in self.classifiers_.items():
                 clf.set_params(random_state=random_state)
-                for oversampling_method_ind, oversampling_method in enumerate(self.oversampling_methods):
-                    if oversampling_method is not None:
-                        oversampling_method.set_params(random_state=random_state)
-                    for scorer_ind, scorer in enumerate(self.scorers_):
-                        for dataset_name, (X, y) in self.datasets.items():
-                            if oversampling_method is not None:
-                                clf = make_pipeline(oversampling_method, clf)
-                            clf_name = self.classifiers_names_[clf_ind]
-                            om_name = self.oversampling_methods_names_[oversampling_method_ind]
-                            metric_name = self.metrics_names_[scorer_ind]
+                for dataset_name, (X, y) in self.datasets_.items():
+                    for oversampling_method_name, oversampling_method in self.oversampling_methods_.items():
+                        if oversampling_method is not None:
+                            oversampling_method.set_params(random_state=random_state)
+                            clf = make_pipeline(oversampling_method, clf)
+                        for metric_name, scorer in self.scorers_.items():
                             cv_score = cross_val_score(clf, X, y, cv=cv, scoring=scorer).mean()
                             msg = 'Experiment: {}\nClassifier: {}\nOversampling method: {}\nMetric: {}\nDataset: {}\nCV score: {}\n\n'
                             if logging_results:
-                                print(msg.format(experiment_ind + 1, clf_name, om_name, metric_name, dataset_name, cv_score))
-                            result = pd.DataFrame([[dataset_name, clf_name, om_name, metric_name, cv_score]], columns=results.columns)
+                                print(msg.format(experiment_ind + 1, classifier_name, oversampling_method_name, metric_name, dataset_name, cv_score))
+                            result = pd.DataFrame([[dataset_name, classifier_name, oversampling_method_name, metric_name, cv_score]], columns=results.columns)
                             results = results.append(result, ignore_index=True)
         
         # Group results dataframe by dataset, classifier and metric
@@ -132,7 +127,10 @@ class BinaryExperiment:
 
         # Calculate mean and std results
         self.mean_cv_results_ = grouped_results.mean().reset_index().rename(columns={'CV score': 'Mean CV score'})
-        self.std_cv_results_ = grouped_results.std().reset_index().rename(columns={'CV score': 'Std CV score'})
+        if self.experiment_repetitions > 1:
+            self.std_cv_results_ = grouped_results.std().reset_index().rename(columns={'CV score': 'Std CV score'})
+        else:
+            self.std_cv_results_ = "Standard deviation is not calculated. More than one experiment repetition is needed."
         
         # Transform mean results to wide format
         mean_cv_results_wide = self.mean_cv_results_.pivot_table(index=['Dataset', 'Classifier', 'Metric'], columns=['Oversampling method'], values='Mean CV score').reset_index()
