2c6a266481f153a81089836820ff71a1afb47af3
gdouzas <gdouzas@icloud.com>
2017-05-23 20:31:45 +0300

    Added export module

---
+++ imbtools/export.py
@@ -0,0 +1,9 @@
+def export_table(df, docx_path):
+    doc = docx.Document(docx_path)
+    table = doc.add_table(df.shape[0]+1, df.shape[1])
+    for j in range(df.shape[1]):
+        table.cell(0, j).text = df.columns[j]
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            table.cell(i+1, j).text = str(df.values[i,j])
+    doc.save(docx_path)
