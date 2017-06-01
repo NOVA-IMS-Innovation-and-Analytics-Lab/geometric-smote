b3ef859f9e96a70a356d6bfaf2c9175c7e61cc62
gdouzas <gdouzas@icloud.com>
2017-06-01 18:30:59 +0300

    Added class initializer

--- imbtools/algorithms/cgan_oversampler.py
+++ imbtools/algorithms/cgan_oversampler.py
@@ -17,3 +17,16 @@ class CGANOversampler(BaseBinarySampler):
     """Class to perform oversampling using a 
     Conditional Generative Adversarial Network as 
     an oversampling algorithm."""
+
+def __init__(self,
+             n_Z_features, 
+             discriminator_hidden_layers, 
+             generator_hidden_layers, 
+             discriminator_optimizer=OPTIMIZER,  
+             discriminator_weights_initilization_choice='xavier',
+             discriminator_bias_initilization_choice='zeros',
+             generator_optimizer=OPTIMIZER, 
+             generator_weights_initilization_choice='xavier',
+             generator_bias_initilization_choice='zeros', 
+             random_state=None):
+    
