9c7b3131d607df9197e18dd7f08b5e7f5f77fc1a
gdouzas <gdouzas@icloud.com>
2017-09-01 02:37:51 +0300

    Made pylint compatible

--- imbtools/evaluation.py
+++ imbtools/evaluation.py
@@ -1,26 +1,26 @@
 """
-This module contains classes to compare and evaluate 
+This module contains classes to compare and evaluate
 the performance of various oversampling algorithms.
 """
 
 # Author: Georgios Douzas <gdouzas@icloud.com>
 
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import StratifiedKFold, GridSearchCV
-from sklearn.model_selection import cross_validate
-from imblearn.pipeline import Pipeline
-from imblearn.metrics import geometric_mean_score
+from warnings import filterwarnings
 from itertools import product
-from .utils import check_datasets, check_random_states, check_models
 from os.path import join
 from os import listdir
 from re import match, sub
+from pickle import dump, load
+from .utils import check_datasets, check_random_states, check_models
+from imblearn.pipeline import Pipeline
+from imblearn.metrics import geometric_mean_score
 from scipy.stats import friedmanchisquare
 from progressbar import ProgressBar
-from pickle import dump, load
 from .metrics import SCORERS
-from warnings import filterwarnings
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from sklearn.model_selection import cross_validate
+import numpy as np
+import pandas as pd
 
 
 def read_csv_dir(dirpath):
@@ -28,66 +28,77 @@ def read_csv_dir(dirpath):
     datasets = []
     csv_files = [csv_file for csv_file in listdir(dirpath) if match('^.+\.csv$', csv_file)]
     for csv_file in csv_files:
-        dataset = pd.read_csv(join(dirpath,csv_file))
+        dataset = pd.read_csv(join(dirpath, csv_file))
         X, y = dataset.iloc[:, :-1], dataset.iloc[:, -1]
         dataset_name = sub(".csv", "", csv_file)
-        datasets.append( (dataset_name, (X, y)) )
+        datasets.append((dataset_name, (X, y)))
     return datasets
 
 def summarize_datasets(datasets):
     """Creates a summary of the datasets."""
     datasets = check_datasets(datasets)
-    summary_columns = ["Dataset name", "# of features", "# of instances", "# of minority instances", "# of majority instances", "Imbalance Ratio"]
+    summary_columns = ["Dataset name",
+                       "# of features",
+                       "# of instances",
+                       "# of minority instances",
+                       "# of majority instances",
+                       "Imbalance Ratio"]
     datasets_summary = pd.DataFrame({}, columns=summary_columns)
     for dataset_name, (X, y) in datasets:
         n_instances = ((y == 0).sum(), (y == 1).sum())
-        dataset_summary = pd.DataFrame([[dataset_name, X.shape[1], y.size, n_instances[1], n_instances[0], round(n_instances[0] / n_instances[1], 2)]], columns=datasets_summary.columns)
+        dataset_summary = pd.DataFrame([[dataset_name,
+                                         X.shape[1],
+                                         y.size,
+                                         n_instances[1],
+                                         n_instances[0],
+                                         round(n_instances[0] / n_instances[1], 2)]],
+                                       columns=datasets_summary.columns)
         datasets_summary = datasets_summary.append(dataset_summary, ignore_index=True)
     datasets_summary[datasets_summary.columns[1:-1]] = datasets_summary[datasets_summary.columns[1:-1]].astype(int)
     return datasets_summary
 
 def _calculate_stats(experiment):
     """Calculates stats for positive and negative scorers."""
-    grouped_results = experiment.results_.groupby(experiment.results_.columns[:-1].tolist(), as_index = False)
+    grouped_results = experiment.results_.groupby(experiment.results_.columns[:-1].tolist(), as_index=False)
     stats = grouped_results.agg({'CV score': [np.mean, np.std]})
     stats.columns = experiment.results_.columns.tolist()[:-1] + ['Mean CV score', 'Std CV score']
     return stats
 
 def calculate_stats(experiment):
-    """Calculates mean and standard deviation across experiments for every 
+    """Calculates mean and standard deviation across experiments for every
     combination of datasets, classifiers, oversamplers and metrics."""
     stats = _calculate_stats(experiment)
     stats['Mean CV score'] = np.abs(stats['Mean CV score'])
     return stats
 
 def calculate_optimal_stats(experiment):
-    """Calculates the highest mean and standard deviation for every 
-    combination of classfiers and oversamplers across different 
+    """Calculates the highest mean and standard deviation for every
+    combination of classfiers and oversamplers across different
     hyperparameters' configurations."""
-    
+
     # Classifiers names
     clfs_names = [clf_name for clf_name, *_ in experiment.classifiers]
     expanded_clfs_names = [clf_name for clf_name, _ in experiment.classifiers_]
-    
+     
     # Oversamplers names
     oversamplers_names = [oversampler_name for oversampler_name, *_ in experiment.oversamplers]
     expanded_oversamplers_names = [oversampler_name for oversampler_name, _ in experiment.oversamplers_]
-    
+
     # Calculate stats table
     stats = _calculate_stats(experiment)
     optimal_stats = pd.DataFrame(columns=stats.columns)
-    
+
     # Populate optimal stats table
     for dataset_name, clf_name, oversampler_name in product(experiment.datasets_names_, clfs_names, oversamplers_names):
         matched_clfs_names = [exp_clf_name for exp_clf_name in expanded_clfs_names if match(clf_name, exp_clf_name)]
         matched_oversamplers_names = [exp_oversampler_name for exp_oversampler_name in expanded_oversamplers_names if match(oversampler_name, exp_oversampler_name)]
-        
+
         is_matched_clfs = np.isin(stats['Classifier'], matched_clfs_names)
         is_matched_oversamplers = np.isin(stats['Oversampler'], matched_oversamplers_names)
         is_matched_dataset = (stats['Dataset'] == dataset_name)
-        
+
         matched_stats = stats[is_matched_clfs & is_matched_oversamplers & is_matched_dataset]
-        optimal_matched_stats = matched_stats.groupby('Metric', as_index = False).agg({'Mean CV score': [max, lambda col: matched_stats['Std CV score'][np.argmax(col)]]})
+        optimal_matched_stats = matched_stats.groupby('Metric', as_index=False).agg({'Mean CV score': [max, lambda col: matched_stats['Std CV score'][np.argmax(col)]]})
         optimal_matched_stats.columns = stats.columns[-3:]
         optimal_matched_names = pd.DataFrame([[dataset_name, clf_name, oversampler_name]] * len(experiment.scoring), columns=stats.columns[:-3])
         optimal_matched_stats = pd.concat([optimal_matched_names, optimal_matched_stats], axis=1)
@@ -97,19 +108,19 @@ def calculate_optimal_stats(experiment):
     return optimal_stats
 
 def calculate_optimal_stats_wide(experiment):
-    """Calculates in wide format the highest mean and standard 
-    deviation for every combination of classfiers and oversamplers 
+    """Calculates in wide format the highest mean and standard
+    deviation for every combination of classfiers and oversamplers
     across different hyperparameters' configurations."""
     optimal_stats = calculate_optimal_stats(experiment)
-    
+
     # Calculate wide format of mean cv
     optimal_mean_wide = optimal_stats.pivot_table(index=['Dataset', 'Classifier', 'Metric'], columns=['Oversampler'], values='Mean CV score').reset_index()
     optimal_mean_wide.columns.rename(None, inplace=True)
-    
+
     # Calculate wide format of std cv
     optimal_std_wide = optimal_stats.pivot_table(index=['Dataset', 'Classifier', 'Metric'], columns=['Oversampler'], values='Std CV score').reset_index()
     optimal_std_wide.columns.rename(None, inplace=True)
-    
+
     # Polpulate wide format of optimal stats
     oversamplers_names = [oversampler_name for oversampler_name, *_ in experiment.oversamplers]
     optimal_stats_wide = pd.DataFrame(columns=oversamplers_names)
@@ -129,7 +140,7 @@ def calculate_mean_ranking(experiment):
     return ranking.groupby(['Classifier', 'Metric'], as_index=False).mean()
 
 def calculate_friedman_test(experiment):
-    """Calculates the friedman test across datasets for every 
+    """Calculates the friedman test across datasets for every
     combination of classifiers and metrics."""
     if len(experiment.oversamplers_) < 3:
         raise ValueError('Friedman test can not be applied. More than two oversampling methods are needed.')
@@ -146,14 +157,14 @@ def load_experiment(filename):
 
 
 class BinaryExperiment:
-    """Class for comparison of oversampling algorithms performance 
+    """Class for comparison of oversampling algorithms performance
     on imbalanced binary classification problems.
-    
+
     Parameters
     ----------
     datasets : list of (X, y) tuples or dictionary of dataset-name:(X,y) pairs
-        The list of (X, y) pairs is a list of tuples of input data and 
-        target values, The dictionary extends the list by adding the datasets names 
+        The list of (X, y) pairs is a list of tuples of input data and
+        target values, The dictionary extends the list by adding the datasets names
         as a key.
     classifiers : list of classifiers
         A list of classifiers.
@@ -174,13 +185,13 @@ class BinaryExperiment:
         The number of CPUs to use to do the computation. -1 means ‘all CPUs’.
     """
 
-    def __init__(self, 
+    def __init__(self,
                  datasets,
                  classifiers,
-                 oversamplers, 
+                 oversamplers,
                  scoring=['roc_auc', 'f1'],
-                 n_splits=3, 
-                 experiment_repetitions=5, 
+                 n_splits=3,
+                 experiment_repetitions=5,
                  random_state=None,
                  n_jobs=1):
         self.datasets = datasets
@@ -191,20 +202,26 @@ class BinaryExperiment:
         self.experiment_repetitions = experiment_repetitions
         self.random_state = random_state
         self.n_jobs = n_jobs
-    
+
     def run(self, hide_warnings=True):
-        """Runs the experimental procedure and calculates the cross validation 
+        """Runs the experimental procedure and calculates the cross validation
         scores for each classifier, oversampling method, datasets and metric."""
-        filterwarnings('ignore') if hide_warnings else filterwarnings('default')
-        
+
+        # Remove warnings
+        if hide_warnings:
+            filterwarnings('ignore')
+        else:
+            filterwarnings('default')
+
         # Initialize experiment parameters
-        if not hasattr(self, 'datasets'): return
+        if not hasattr(self, 'datasets'):
+            return
         datasets = check_datasets(self.datasets)
         self.datasets_names_ = [dataset_name for dataset_name, _ in datasets]
         self.random_states_ = check_random_states(self.random_state, self.experiment_repetitions)
         self.classifiers_ = check_models(self.classifiers, "classifier")
         self.oversamplers_ = check_models(self.oversamplers, "oversampler")
-        
+
         # Initialize progress bar
         bar = ProgressBar(redirect_stdout=False, max_value=len(self.random_states_) * len(datasets) * len(self.classifiers_) * len(self.oversamplers_))
         iterations = 0
@@ -212,7 +229,7 @@ class BinaryExperiment:
         # Populate results dataframe
         results_columns = ['Dataset', 'Classifier', 'Oversampler', 'Metric', 'CV score']
         self.results_ = pd.DataFrame(columns=results_columns)
-        for experiment_ind, random_state in enumerate(self.random_states_):
+        for random_state in self.random_states_:
             cv = StratifiedKFold(n_splits=self.n_splits, random_state=random_state, shuffle=True)
             for dataset_name, (X, y) in datasets:
                 for clf_name, clf, in self.classifiers_:
