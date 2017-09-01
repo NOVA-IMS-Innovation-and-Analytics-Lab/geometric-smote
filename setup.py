c4f5a41b3c5a64d420ba05c4bec81259502d70ab
Felix Last <mail@felixlast.de>
2017-09-01 17:30:01 +0200

    Bump version due to incompatible API changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="imbalanced-tools",
-    version="0.0.1",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'docx',
