42644f411b9013028f157ce9fe8a27e225bd3eec
georgedouzas <georgios.douzas@gmail.com>
2018-02-06 19:22:55 +0200

    Package name was corrected

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hyperparam-learn",
+    name="sklearn-extensions",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
