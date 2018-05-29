354ec7f373632b9781036e707ca0b0a33378d9eb
Plapa <lapadsk@hotmail.com>
2018-05-29 22:16:35 +0100

    complete test for shape

--- sklearnext/preprocessing/over_sampling/tests/test_dbscan_smote.py
+++ sklearnext/preprocessing/over_sampling/tests/test_dbscan_smote.py
@@ -11,12 +11,12 @@ def test_random_seed(RND_SEED):
 
 
 X = np.array([[1,1], [0,0], [0,1], [0, -1], [-1, 0], [1,1], [0, 0], [0, 1], [0, -1], [-1, 0], [-1, -1], [1, 1], [0, -1], [-1, 0], [-1, -1], [1, 1]])
-y = np.array([0, 0, 0, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
-y_expected = ([0, 0, 0, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+y_expected = ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
 @pytest.mark.parametrize("X, y, y_expected", [(X, y, y_expected)])
 def test_correct_y_shape(X, y, y_expected):
     dbsn = DBSCANSMOTE()
     X_, y_ = dbsn.fit_sample(X, y)
 
-    assert y_.shape[0] == y_expected.shape[0]
+    assert y_.shape[0] == np.shape(y_expected)[0]
