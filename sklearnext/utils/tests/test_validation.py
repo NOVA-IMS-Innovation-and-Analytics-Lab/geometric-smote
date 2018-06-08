aca343cab78f30a04bba5a928966136224135696
georgedouzas <georgios.douzas@gmail.com>
2018-06-08 18:13:06 +0300

    Fixed bug to check of param grids

--- sklearnext/utils/tests/test_validation.py
+++ sklearnext/utils/tests/test_validation.py
@@ -4,25 +4,51 @@ Test the data module.
 
 import pytest
 from sklearn.datasets import make_regression
-from ..validation import _check_param_grid, check_param_grids, check_datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVR
+from ..validation import _normalize_param_grid, check_param_grids, check_datasets
 
 X, y = make_regression()
+ESTIMATORS = [
+    ('lr', LinearRegression()),
+    ('svr', SVR()),
+    ('pip', Pipeline([('scaler', MinMaxScaler()), ('lr', LinearRegression())]))
+]
+PARAM_GRIDS = [
+    {'lr__normalize': [True, False], 'lr__fit_intercept': [True, False]},
+    {'svr__C': [0.01, 0.1, 1.0], 'svr__kernel': ['rbf', 'linear']},
+    {'pip__scaler__feature_range': [(0, 1), (0, 10)], 'pip__lr__normalize': [True, False]}
+]
+UPDATED_PARAM_GRIDS = [
+    {'lr__normalize': [True, False], 'lr__fit_intercept': [True, False], 'est_name':['lr']},
+    {'svr__C': [0.01, 0.1, 1.0], 'svr__kernel': ['rbf', 'linear'], 'est_name':['svr']},
+    {'pip__scaler__feature_range': [(0, 1), (0, 10)], 'pip__lr__normalize': [True, False], 'est_name':['pip']}
+]
 
 
-def test_check_param_grid():
-    param_grid = {'est__param': [1, 2, 3], 'est__param__subparam': [4, 5, 6]}
-    updated_param_grid = param_grid.copy()
-    updated_param_grid.update({'est_name': ['est']})
-    assert _check_param_grid(param_grid) == updated_param_grid
+@pytest.mark.parametrize('param_grid,updated_param_grid', [
+    (PARAM_GRIDS[0], UPDATED_PARAM_GRIDS[0]),
+    (PARAM_GRIDS[1], UPDATED_PARAM_GRIDS[1]),
+    (PARAM_GRIDS[2], UPDATED_PARAM_GRIDS[2]),
+    ({}, {'est_name': []})
+])
+def test_check_normalize_grid(param_grid, updated_param_grid):
+    assert _normalize_param_grid(param_grid) == updated_param_grid
 
 
-def test_check_param_grids():
-    param_grids = [{'est1__param': [1, 2, 3], 'est1__param__subparam': [4, 5, 6]},
-                   {'est2__param': [10, 20, 30], 'est2__param__subparam': [40, 50, 60]}]
-    updated_param_grids = [{'est1__param': [1, 2, 3], 'est1__param__subparam': [4, 5, 6], 'est_name': ['est1']},
-                           {'est2__param': [10, 20, 30], 'est2__param__subparam': [40, 50, 60], 'est_name': ['est2']}]
-
-    assert check_param_grids(param_grids) == updated_param_grids
+@pytest.mark.parametrize('param_grids,updated_param_grids,estimators', [
+    (PARAM_GRIDS, UPDATED_PARAM_GRIDS, ESTIMATORS),
+    (PARAM_GRIDS[0:2], UPDATED_PARAM_GRIDS[0:2] + [{'est_name': ['pip']}], ESTIMATORS),
+    (PARAM_GRIDS[0:1], UPDATED_PARAM_GRIDS[0:1] + [{'est_name': ['svr']}, {'est_name': ['pip']}], ESTIMATORS),
+    (PARAM_GRIDS[0:1] + PARAM_GRIDS[2:], UPDATED_PARAM_GRIDS[0:1] + UPDATED_PARAM_GRIDS[2:] + [{'est_name': ['svr']}], ESTIMATORS),
+    ({}, [{'est_name': ['lr']}, {'est_name': ['svr']}, {'est_name': ['pip']}], ESTIMATORS)
+])
+def test_check_param_grids(param_grids, updated_param_grids, estimators):
+    checked_param_grids = check_param_grids(param_grids, estimators)
+    assert len(checked_param_grids) == len(updated_param_grids)
+    assert all(param_grid in checked_param_grids for param_grid in updated_param_grids)
 
 
 @pytest.mark.parametrize('dataset_name1,dataset_name2,data', [
--- sklearnext/utils/validation.py
+++ sklearnext/utils/validation.py
@@ -6,27 +6,39 @@ for input validation.
 # Author: Georgios Douzas <gdouzas@icloud.com>
 # License: BSD 3 clause
 
+from sklearn.base import BaseEstimator
 from sklearn.utils import check_random_state
+from sklearn.model_selection._search import _check_param_grid
 
 
-def _check_param_grid(param_grid):
+def _normalize_param_grid(param_grid):
     """Normalize the parameter grid to use with
     parametrized estimators."""
+    _check_param_grid(param_grid)
     normalized_param_grid = param_grid.copy()
     est_name = list(set([param.split('__')[0] for param in param_grid.keys()]))
     normalized_param_grid.update({'est_name': est_name})
     return normalized_param_grid
 
 
-def check_param_grids(param_grids):
+def check_param_grids(param_grids, estimators):
     """Normalize the parameter grid to use with
     parametrized estimators."""
     if isinstance(param_grids, list):
         normalized_param_grids = []
         for param_grid in param_grids:
-            normalized_param_grids.append(_check_param_grid(param_grid))
+            normalized_param_grids.append(_normalize_param_grid(param_grid))
     else:
-        normalized_param_grids = _check_param_grid(param_grids)
+        normalized_param_grids = [_normalize_param_grid(param_grids)]
+    est_names, _ = zip(*estimators)
+    est_names = set(est_names)
+    try:
+        generated_est_names = set([param_grid['est_name'][0] for param_grid in normalized_param_grids])
+    except IndexError:
+        generated_est_names = set()
+        normalized_param_grids = []
+    for est_name in est_names.difference(generated_est_names):
+        normalized_param_grids += [{'est_name': [est_name]}]
     return normalized_param_grids
 
 
@@ -49,6 +61,15 @@ def check_random_states(random_state, repetitions):
     random_state = check_random_state(random_state)
     return [random_state.randint(0, 2 ** 32 - 1, dtype='uint32') for _ in range(repetitions)]
 
+
+def check_estimators(estimators):
+    """Check estimators correct input."""
+    error_msg = 'Invalid `estimators` attribute, `estimators` should be a list of (string, estimator) tuples.'
+    try:
+        if not all([all([isinstance(name, str), isinstance(est, BaseEstimator)]) for name, est in estimators]) or len(estimators) == 0:
+            raise AttributeError(error_msg)
+    except:
+        raise AttributeError(error_msg)
 
 
 
