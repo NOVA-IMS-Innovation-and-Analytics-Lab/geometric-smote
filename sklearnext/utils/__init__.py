26e8d7e207a05e3cc5f4d27695c62a4d50d39d8e
georgedouzas <georgios.douzas@gmail.com>
2018-06-10 19:36:09 +0300

    Added new check function to __all__ imports

--- sklearnext/utils/__init__.py
+++ sklearnext/utils/__init__.py
@@ -2,6 +2,6 @@
 The :mod:`sklearn_extensions.utils` module includes various utilities.
 """
 
-from .validation import check_param_grids, check_datasets, check_random_states
+from .validation import check_param_grids, check_datasets, check_random_states, check_oversamplers_classifiers
 
-__all__ = ['check_param_grids', 'check_datasets', 'check_random_states']
+__all__ = ['check_param_grids', 'check_datasets', 'check_random_states', 'check_oversamplers_classifiers']
