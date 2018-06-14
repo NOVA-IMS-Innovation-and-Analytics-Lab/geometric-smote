b8ac321091764ef5ddabdac66adde02e714ff969
georgedouzas <georgios.douzas@gmail.com>
2018-06-14 17:57:07 +0300

    Initial tests

--- sklearnext/tools/tests/test_imbalanced_analysis.py
+++ sklearnext/tools/tests/test_imbalanced_analysis.py
@@ -7,14 +7,25 @@ from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
+from ..imbalanced_analysis import evaluate_imbalanced_experiments
 
-X, y = make_classification(weights=[0.90, 0.10])
+X1, y1 = make_classification(weights=[0.90, 0.10], n_samples=100)
+X2, y2 = make_classification(weights=[0.70, 0.30], n_samples=80)
+DATASETS = [('A',(X1, y1)), ('B', (X2, y2))]
 OVERSAMPLERS = [
     ('random', RandomOverSampler()),
-    ('smote', SMOTE(), {'k_neighbors': [2, 3, 4], 'kind': ['regular', 'borderline1']}),
-    ('adasyn', ADASYN(), {'n_neighbors': [2, 3, 4, 5]})
+    ('smote', SMOTE(), {'k_neighbors': [2, 3], 'kind': ['regular', 'borderline1']}),
+    ('adasyn', ADASYN(), {'n_neighbors': [2, 3, 4]})
 ]
 CLASSIFIERS = [
     ('lr', LogisticRegression()),
-    ('svc', SVC(), {'C': [0.1, 0.5, 1.0], 'kernel': ['rbf', 'linear']})
+    ('svc', SVC(), {'C': [0.1, 1.0]})
 ]
+
+
+@pytest.mark.parametrize('scoring,n_runs', [
+    ('f1', 3)
+])
+def test_evaluate_imbalanced_experiment(scoring, n_runs):
+    """Test the output of the model search report function."""
+    evaluation = evaluate_imbalanced_experiments(DATASETS, OVERSAMPLERS, CLASSIFIERS, scoring, n_runs=n_runs)
