499c763b22e980d5add16b085d11915b3cd1c6d2
georgedouzas <georgios.douzas@gmail.com>
2018-06-16 12:59:19 +0300

    Modified for easier importing

--- sklearnext/tools/__init__.py
+++ sklearnext/tools/__init__.py
@@ -3,7 +3,12 @@ The :mod:`sklearn_extensions.tools` module includes various functions
 to analyze and visualize the results of experiments.
 """
 
-from .imbalanced_analysis import evaluate_binary_imbalanced_experiments
+from .imbalanced_analysis import evaluate_binary_imbalanced_experiments, read_csv_dir, summarize_binary_datasets
 from .model_analysis import report_model_search_results
 
-__all__ = ['evaluate_binary_imbalanced_experiments', 'report_model_search_results']
+__all__ = [
+    'evaluate_binary_imbalanced_experiments',
+    'report_model_search_results',
+    'read_csv_dir',
+    'summarize_binary_datasets'
+]
