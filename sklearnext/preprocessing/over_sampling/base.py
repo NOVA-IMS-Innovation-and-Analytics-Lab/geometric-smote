8c9f8d953d3ec41d5b5dae66a742c9014e65c1c3
georgedouzas <georgios.douzas@gmail.com>
2018-06-24 17:06:12 +0300

    Oversampling method is now similar to clustering oversampling

--- sklearnext/preprocessing/over_sampling/base.py
+++ sklearnext/preprocessing/over_sampling/base.py
@@ -7,12 +7,25 @@ Extended base class for oversampling.
 
 from math import ceil
 from abc import abstractmethod
+from collections import Counter
 import numpy as np
 import pandas as pd
 from sklearn.utils import check_random_state
 from imblearn.over_sampling.base import BaseOverSampler
 
-CATEGORICAL_STRATEGY = ('most_frequent', 'less_frequent', 'random')
+
+def _generate_classes_stats(y, majority_label, imbalance_ratio_threshold, k_neighbors):
+    """Generate stats for the various minority classes."""
+    counter = Counter(y)
+    stats = {label:((counter[majority_label]/ n_samples), n_samples) for label, n_samples in counter.items()
+             if label != majority_label}
+    include_group = any([ir < imbalance_ratio_threshold and n_samples > k_neighbors
+                         if k_neighbors is not None else ir < imbalance_ratio_threshold
+                         for label, (ir, n_samples) in stats.items()])
+    modified_imbalance_ratios = {label: ((counter[majority_label] + 1) / (n_samples + 1))
+                                 for label, n_samples in counter.items()
+                                 if label != majority_label}
+    return include_group, modified_imbalance_ratios
 
 
 class ExtendedBaseOverSampler(BaseOverSampler):
@@ -28,15 +41,13 @@ class ExtendedBaseOverSampler(BaseOverSampler):
                  random_state=None,
                  sampling_type=None,
                  categorical_cols=None,
-                 categorical_threshold=1.0,
-                 categorical_strategy='most_frequent'):
+                 imbalance_ratio_threshold=1.0):
         super(ExtendedBaseOverSampler, self).__init__(ratio, random_state, sampling_type)
         self.categorical_cols = categorical_cols
-        self.categorical_threshold = categorical_threshold
-        self.categorical_strategy = categorical_strategy
+        self.imbalance_ratio_threshold = imbalance_ratio_threshold
 
     @abstractmethod
-    def _group_sample(self, X, y):
+    def _partial_sample(self, X, y):
         """Resample the numerical features of the dataset.
 
         Parameters
@@ -80,7 +91,8 @@ class ExtendedBaseOverSampler(BaseOverSampler):
         self.random_state_ = check_random_state(self.random_state)
 
         if self.categorical_cols is None:
-            return self._group_sample(X, y)
+            return self._partial_sample(X, y)
+
         max_col_index = X.shape[1]
         try:
             if len(self.categorical_cols) == 0 or not set(range(max_col_index)).issuperset(self.categorical_cols):
@@ -89,33 +101,36 @@ class ExtendedBaseOverSampler(BaseOverSampler):
         except:
             raise ValueError('Parameter `categorical_cols` should be a list or tuple in the %s range.' % [0, max_col_index])
 
-        if self.categorical_strategy not in CATEGORICAL_STRATEGY:
-            error_msg = 'Unknown categorical strategy. Choices are {}. Got {} instead.'
-            raise ValueError(error_msg.format(CATEGORICAL_STRATEGY, self.categorical_strategy))
-
-        if self.categorical_threshold <= 0.0 or self.categorical_threshold > 1.0:
-            raise ValueError('Parameter `categorical_threshold` should be in the (0, 1] range.')
+        if self.imbalance_ratio_threshold <= 0.0:
+            raise ValueError('Parameter `categorical_threshold` should be a positive number.')
 
         df = pd.DataFrame(np.column_stack((X, y)))
 
-        # Count samples in each group
-        n_samples_groups = df.groupby(self.categorical_cols).size().sort_values(ascending=False)
+        # Select groups to oversample
+        majority_label = [label for label, n_samples in self.ratio_.items() if n_samples == 0][0]
+        minority_labels = [label for label in self.ratio_.keys() if label != majority_label]
+        classes_stats = df.groupby(self.categorical_cols, as_index=False).agg(
+            {
+                df.columns[-1]: lambda y: _generate_classes_stats(
+                    y,
+                    majority_label,
+                    self.imbalance_ratio_threshold,
+                    self.k_neighbors if hasattr(self, 'k_neighbors') else None
+                )
+            }
+        )
+        boolean_mask = classes_stats.iloc[:, -1].apply(lambda stat: stat[0])
+        included_groups = classes_stats[boolean_mask].iloc[:, :-1].reset_index(drop=True)
+        self.n_overasmpled_groups_ = len(included_groups)
 
-        # Select number of groups based on threshold
-        n_groups = ceil(len(n_samples_groups) * self.categorical_threshold)
-
-        # Define data to exclude from oversampling
-        excluded_groups = pd.DataFrame(n_samples_groups[n_groups:]).reset_index().iloc[:, :-1]
-        df_excluded = pd.merge(df, excluded_groups, how='right')
-        X_excluded, y_excluded = df_excluded.iloc[:, :-1], df_excluded.iloc[:, -1:]
-
-        # Selected groups to oversample data based on different strategies
-        n_samples_groups = n_samples_groups[:n_groups]
-        weights = n_samples_groups / n_samples_groups.sum()
-        if self.categorical_strategy == 'less_frequent':
-            weights[:] = weights.values[::-1]
-        elif self.categorical_strategy == 'random':
-            weights = weights.sample(weights.size, random_state=self.random_state_)
+        # Calculate oversampling weights
+        imbalance_ratios = classes_stats[boolean_mask].iloc[:, -1].apply(lambda stat: stat[1]).reset_index(drop=True)
+        weights = pd.DataFrame()
+        for label in minority_labels:
+            label_weights = imbalance_ratios.apply(lambda ratio: ratio.get(label, np.nan))
+            label_weights = label_weights / label_weights.sum()
+            label_weights.rename(label, inplace=True)
+            weights = pd.concat([weights, label_weights], axis=1)
 
         initial_ratio = self.ratio_.copy()
 
@@ -123,17 +138,18 @@ class ExtendedBaseOverSampler(BaseOverSampler):
         y_resampled = pd.DataFrame(columns=[df.columns[-1]])
 
         # Oversample data in each group
-        for group_values, weight in weights.items():
+        for group_values, (_, weight) in zip(included_groups.values.tolist(), weights.iterrows()):
 
             # Define ratio in group
-            self.ratio_ = {class_label: round(n_samples * weight) for class_label, n_samples in initial_ratio.items()}
+            self.ratio_ = {label: (int(n_samples * weight[label]) if label != majority_label else n_samples)
+                           for label, n_samples in initial_ratio.items()}
 
             # Select data in group
             df_group = pd.merge(df, pd.DataFrame([group_values], columns=self.categorical_cols)).drop(columns=self.categorical_cols)
             X_group, y_group = df_group.iloc[:, :-1], df_group.iloc[:, -1]
 
             # Oversample data
-            X_group_resampled, y_group_resampled = self._group_sample(X_group.values, y_group.values)
+            X_group_resampled, y_group_resampled = self._partial_sample(X_group.values, y_group.values)
             X_group_categorical = np.array(group_values * len(X_group_resampled)).reshape(len(X_group_resampled), -1)
             X_group_resampled = np.column_stack((X_group_resampled, X_group_categorical))
             X_group_resampled = pd.DataFrame(X_group_resampled, columns=list(X_group.columns) + self.categorical_cols)
@@ -147,8 +163,10 @@ class ExtendedBaseOverSampler(BaseOverSampler):
         self.ratio_ = initial_ratio.copy()
 
         # Append excluded data
-        X_resampled = X_resampled.append(X_excluded).values
-        y_resampled = y_resampled.append(y_excluded).values.reshape(-1)
+        excluded_groups = classes_stats[~boolean_mask].iloc[:, :-1].reset_index(drop=True)
+        df_excluded = pd.merge(df, excluded_groups)
+        X_resampled = X_resampled.append(df_excluded.iloc[:, :-1]).values
+        y_resampled = y_resampled.append(df_excluded.iloc[:, -1:]).values.reshape(-1)
 
         return X_resampled, y_resampled
 
