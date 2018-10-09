b371984b28834baf670085778d9b07a5361fef80
georgedouzas <georgios.douzas@gmail.com>
2018-10-09 09:09:55 +0300

    Removed clustering oversamplers from importing

--- sklearnext/over_sampling/__init__.py
+++ sklearnext/over_sampling/__init__.py
@@ -4,15 +4,9 @@ The :mod:`sklearnext.oversampling` module includes
 """
 
 from .cgan_oversampler import CGANOversampler
-from .dbscan_smote import DBSCANSMOTE
 from .geometric_smote import GeometricSMOTE
-from .kmeans_smote import KMeansSMOTE
-from .somo import SOMO
 
 __all__ = [
     'CGANOversampler',
-    'DBSCANSMOTE',
-    'GeometricSMOTE',
-    'KMeansSMOTE',
-    'SOMO'
+    'GeometricSMOTE'
 ]
