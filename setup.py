17788e70b9ac8ff3b2bd5e146b83e173ad12dc39
georgedouzas <georgios.douzas@gmail.com>
2018-10-20 19:16:06 +0300

    Added packages versions

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sklearnext",
-    version="0.1.0",
+    name='sklearnext',
+    python_requires='>3.5',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'scipy',
-        'numpy',
-        'pandas',
-        'matplotlib',
-        'scikit-learn',
-        'imbalanced-learn',
-        'dask_ml',
-        'progressbar2'
+        'scipy==1.1.0',
+        'statsmodels==0.8.0',
+        'numpy==1.14.5',
+        'pandas==0.23.1',
+        'scikit-learn==0.19.1',
+        'imbalanced-learn==0.3.3',
+        'dask-searchcv==0.2.0',
+        'progressbar2==3.38.0'
     ]
 )
