5ea464fa9a60ad4a2d082442fbda90efbdd7804a
georgedouzas <georgios.douzas@gmail.com>
2018-10-31 04:31:49 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     packages=find_packages(),
     install_requires=[
         'scipy==1.1.0',
-        'statsmodels==0.8.0',
         'numpy==1.15.3',
         'pandas==0.23.4',
         'scikit-learn==0.20.0',
         'imbalanced-learn==0.4.2',
+        'statsmodels==0.9.0',
         'somoclu==1.7.5',
         'dask-searchcv==0.2.0',
         'progressbar2==3.38.0'
