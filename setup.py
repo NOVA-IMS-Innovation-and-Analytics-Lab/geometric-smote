3f06feb95694b6293d15142991dfa2da893cb3fe
georgedouzas <georgios.douzas@gmail.com>
2019-05-16 01:23:08 +0300

    Updated packages

--- setup.py
+++ setup.py
@@ -6,14 +6,12 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'scipy==1.1.0',
-        'numpy==1.15.3',
-        'pandas==0.23.4',
-        'scikit-learn==0.20.0',
-        'imbalanced-learn==0.4.2',
+        'scipy==1.2.1',
+        'numpy==1.16.3',
+        'pandas==0.24.2',
+        'scikit-learn==0.21.1',
+        'imbalanced-learn==0.4.3',
         'statsmodels==0.9.0',
-        'somoclu==1.7.5',
-        'dask-searchcv==0.2.0',
-        'progressbar2==3.38.0'
+        'somoclu==1.7.5'
     ]
 )
