df3e0d73e3ba8f10adbd1d405b57877580053c3f
georgedouzas <georgios.douzas@gmail.com>
2019-06-22 20:31:27 +0300

    Fixed versions

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'scipy==1.2.1',
-        'numpy==1.16.3',
-        'scikit-learn==0.20.3',
-        'imbalanced-learn==0.4.3'
+        'scipy>=0.17',
+        'numpy>=1.1',
+        'scikit-learn>=0.21',
+        'imbalanced-learn>=0.4.3'
     ]
 )
