2c18decf7a79af64935bcda9d652dcf6be056575
georgedouzas <georgios.douzas@gmail.com>
2019-07-04 02:32:25 +0300

    Modified setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,66 @@
-from setuptools import setup, find_packages
+#! /usr/bin/env python
+"""Geometric SMOTE algorithm."""
+
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+ver_file = os.path.join('gsmote', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+DISTNAME = 'geometric-smote'
+DESCRIPTION = 'Geometric SMOTE algorithm.'
+with codecs.open('README.rst', encoding='utf-8-sig') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'G. Douzas'
+MAINTAINER_EMAIL = 'gdouzas@icloud.com'
+URL = 'https://github.com/AlgoWit/publications'
+LICENSE = 'new BSD'
+DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/project-template'
+VERSION = __version__
+INSTALL_REQUIRES = ['scipy>=0.17', 'numpy>=1.1', 'scikit-learn>=0.21', 'imbalanced-learn>=0.4.3']
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7']
+EXTRAS_REQUIRE = {
+    'tests': [
+        'pytest',
+        'pytest-cov'],
+    'docs': [
+        'sphinx',
+        'sphinx-gallery',
+        'sphinx_rtd_theme',
+        'numpydoc',
+        'matplotlib'
+    ]
+}
 
 setup(
-    name='gsmote',
-    python_requires='>3.5',
-    version='0.1.0',
+    name=DISTNAME,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    download_url=DOWNLOAD_URL,
+    long_description=LONG_DESCRIPTION,
+    zip_safe=False,
+    classifiers=CLASSIFIERS,
     packages=find_packages(),
-    install_requires=[
-        'scipy>=0.17',
-        'numpy>=1.1',
-        'scikit-learn>=0.21',
-        'imbalanced-learn>=0.4.3'
-    ]
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE
 )
