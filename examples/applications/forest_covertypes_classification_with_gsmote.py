ffb049c4ef0ba30a1939a37483aae44541645678
georgedouzas <georgios.douzas@gmail.com>
2019-07-07 00:00:52 +0300

    Reformatted applied example

--- examples/applications/forest_covertypes_classification_with_gsmote.py
+++ examples/applications/forest_covertypes_classification_with_gsmote.py
@@ -7,6 +7,10 @@ The examples makes use of scikit-learn's Forest covertypes dataset.
 
 """
 
+# Authors: Joao Fonseca <jpmrfonseca@gmail.com>
+#          Georgios Douzas <gdouzas@icloud.com>
+# Licence: MIT
+
 from collections import Counter
 
 import pandas as pd
@@ -21,44 +25,50 @@ from gsmote import GeometricSMOTE
 print(__doc__)
 
 
-###################
+###############################################################################
 # Forest covertypes
-#
+###############################################################################
+
+###############################################################################
 # The samples in this dataset correspond to 30×30m patches of forest in the US,
-# collected for the task of predicting each patch's cover type,
-# i.e. the dominant species of tree.
-# There are seven covertypes, making this a multiclass classification problem.
-# Each sample has 54 features, described on the
+# collected for the task of predicting each patch's cover type, i.e. the
+# dominant species of tree. There are seven covertypes, making this a multiclass
+# classification problem. Each sample has 54 features, described on the
 # `dataset's homepage <https://archive.ics.uci.edu/ml/datasets/Covertype>`__.
-# Some of the features are boolean indicators,
-# while others are discrete or continuous measurements.
+# Some of the features are boolean indicators, while others are discrete or
+# continuous measurements.
 
-# :func:`sklearn.datasets.fetch_covtype` will load the covertype dataset;
-# it returns a dictionary-like object
-# with the feature matrix in the ``data`` member
-#and the target values in ``target``.
-# The dataset will be downloaded from the web if necessary.
+###############################################################################
+# Dataset
+###############################################################################
 
-# This dataset is clearly imbalanced::
- 
-# Below we have a simple implementation of a Random Forest Classifier to predict
-# the forest type of each patch of forest. Two experiments are ran: One using
-#only the classifier (without oversampling), another using G-SMOTE (put together
-# using a pipeline). Afterwards a classification report is shown for both
-# experiments.
-
-
+###############################################################################
+# The function :func:`sklearn.datasets.fetch_covtype` will load the covertype
+# dataset; it returns a dictionary-like object with the feature matrix in the
+# ``data`` member and the target values in ``target``. The dataset will be
+# downloaded from the web if necessary. This dataset is clearly imbalanced.
 
 X, y, description = datasets.fetch_covtype().values()
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.1)
 print(description)
 
 counts = dict(Counter(y))
-pd.DataFrame(counts.values(), index=counts.keys(), columns=['count']).sort_index()
+print(pd.DataFrame(counts.values(), index=counts.keys(), columns=['count']).sort_index())
+
+
+###############################################################################
+# Classification
+###############################################################################
+
+###############################################################################
+# Below we have a simple implementation of a Random Forest Classifier to predict
+# the forest type of each patch of forest. Two experiments are ran: One using
+# only the classifier (without oversampling), another using G-SMOTE (put
+# together using a pipeline). Afterwards a classification report is shown for
+# both experiments.
 
 clf = RandomForestClassifier(bootstrap=True)
 ovs_clf = make_pipeline(GeometricSMOTE(), RandomForestClassifier(bootstrap=True))
-
 for comb in [clf, ovs_clf]:
     title = f'{comb.__class__.__name__} - Results'
     div = '='*len(title)
