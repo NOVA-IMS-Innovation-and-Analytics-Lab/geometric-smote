1627a38fecf5fdf78dcae6e7feea4c505f79c2e4
georgedouzas <georgios.douzas@gmail.com>
2019-07-08 02:04:54 +0300

    Added conda-recipe

---
+++ conda-recipe/conda_deployment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir build_conda
+conda build build_conda imbalanced-learn
+dirs=`ls -l "$PWD/build_conda"`
+for d in build_conda/*/ ; do
+    for file in $d*
+    do
+        if [[ -f $file ]]; then
+            anaconda upload --user AlgoWit $file
+        fi
+    done
+done
+rm -r build_conda
