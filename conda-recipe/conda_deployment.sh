af334896d8aca95ddfc192a1d9201f62ecde2506
georgedouzas <georgios.douzas@gmail.com>
2019-07-08 12:15:05 +0300

    Build correct directory

--- conda-recipe/conda_deployment.sh
+++ conda-recipe/conda_deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir build_conda
-conda build build_conda imbalanced-learn
+conda build build_conda geometric-smote
 dirs=`ls -l "$PWD/build_conda"`
 for d in build_conda/*/ ; do
     for file in $d*
